0247e0f07ba93801e5f5470755204e4435ac2e61
pilagod <pilagooood@gmail.com>
2023-10-24 16:17:33 +0800

    add options page

---
+++ options.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+import "./style.css"
+
+function OptionsIndex() {
+  const [data, setData] = useState("")
+
+  return (
+    <div className="flex flex-col p-16 text-red-500">
+      <h1>
+        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
+      </h1>
+      <h2>This is the Option UI page!</h2>
+      <input onChange={(e) => setData(e.target.value)} value={data} />
+    </div>
+  )
+}
+
+export default OptionsIndex
