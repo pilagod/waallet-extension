99c42daa80f96cca769cd7152bb9cf7188984e62
pilagod <pilagooood@gmail.com>
2023-12-11 17:40:18 +0800

    extract simple account factory adapter

--- packages/provider/waallet/background/account/eoa.ts
+++ packages/provider/waallet/background/account/eoa.ts
@@ -4,14 +4,17 @@ import type { BigNumberish, HexString } from "~typings"
 
 import { type Account } from "./index"
 
+export interface EoaOwnedAccountFactoryAdapter {
+  getAddress(owner: HexString, salt: BigNumberish): Promise<HexString>
+}
+
 export class EoaOwnedAccount implements Account {
   private owner: ethers.Wallet
 
   private accountAddress?: HexString
 
-  private factory: ethers.Contract
-  private salt: BigNumberish
-  private nodeRpcProvider: ethers.JsonRpcProvider
+  private factoryAdapter?: EoaOwnedAccountFactoryAdapter
+  private salt?: BigNumberish
 
   public constructor(opts: {
     ownerPrivateKey: HexString
@@ -19,10 +22,8 @@ export class EoaOwnedAccount implements Account {
   })
   public constructor(opts: {
     ownerPrivateKey: HexString
-    // TODO: Extract factory strategy
-    factoryAddress: HexString
+    factoryAdapter: EoaOwnedAccountFactoryAdapter
     salt: BigNumberish
-    nodeRpcUrl: string
   })
   public constructor(
     opts:
@@ -32,35 +33,25 @@ export class EoaOwnedAccount implements Account {
         }
       | {
           ownerPrivateKey: HexString
-          factoryAddress: HexString
+          factoryAdapter: EoaOwnedAccountFactoryAdapter
           salt: BigNumberish
-          nodeRpcUrl: string
         }
   ) {
     if ("accountAddress" in opts) {
       this.accountAddress = opts.accountAddress
     }
-    if ("factoryAddress" in opts) {
-      this.factory = new ethers.Contract(opts.factoryAddress, [
-        "function getAddress(address owner, uint256 salt) view returns (address)"
-      ])
+    if ("factoryAdapter" in opts) {
+      this.factoryAdapter = opts.factoryAdapter
       this.salt = opts.salt
-      this.nodeRpcProvider = new ethers.JsonRpcProvider(opts.nodeRpcUrl)
     }
     this.owner = new ethers.Wallet(opts.ownerPrivateKey)
   }
 
   public async getAddress() {
     if (!this.accountAddress) {
-      this.accountAddress = ethers.zeroPadValue(
-        ethers.stripZerosLeft(
-          await this.nodeRpcProvider.call(
-            await this.factory
-              .getFunction("getAddress")
-              .populateTransaction(this.owner.address, this.salt)
-          )
-        ),
-        20
+      this.accountAddress = await this.factoryAdapter.getAddress(
+        this.owner.address,
+        this.salt
       )
     }
     return this.accountAddress
