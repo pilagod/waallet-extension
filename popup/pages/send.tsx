c747347644e9608529069e6920d1defd1cd96940
Irara <irara.chen@token.im>
2024-02-29 19:08:18 +0800

    feat: add send eth page in popup

--- popup/pages/send.tsx
+++ popup/pages/send.tsx
@@ -1,5 +1,11 @@
 import * as ethers from "ethers"
-import { useCallback, useContext, useState, type ChangeEvent } from "react"
+import {
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+  type ChangeEvent
+} from "react"
 import { useLocation } from "wouter"
 
 import { ProviderCtx } from "~popup/ctx/provider"
@@ -9,23 +15,37 @@ import type { BigNumberish, HexString } from "~typing"
 export function Send() {
   const providerCtx = useContext(ProviderCtx)
   const [_, setLocation] = useLocation()
+  const [txHash, setTxHash] = useState<HexString>("")
 
   // Input state
-  const [to, setTo] = useState<string>("")
-  const [amount, setAmount] = useState<string>("0")
+  const [txTo, setTxTo] = useState<HexString>("")
+  const [txToSuggestions, setTxToSuggestions] = useState<HexString[]>([""])
+  const [txValue, setTxValue] = useState<BigNumberish>("0")
   const [toBorderColor, setToBorderColor] = useState<string>("border-gray-300")
   const [amountBorderColor, setAmountBorderColor] =
     useState<string>("border-gray-300")
 
   // Button state
-  const [sendLock, setSendLock] = useState<boolean>(false)
+  const [sendLock, setSendLock] = useState<boolean>(true)
+
+  // Only for test, set the to address as the from address
+  useEffect(() => {
+    const asyncFn = async () => {
+      setTxToSuggestions(
+        (await providerCtx.provider.listAccounts()).map((account) => {
+          return account.address
+        })
+      )
+    }
+    asyncFn()
+  }, [])
 
   const inputChange = useCallback(
     async (event: ChangeEvent<HTMLInputElement>) => {
       const { id, value } = event.target
       switch (id) {
         case InputId.to:
-          setTo(value)
+          setTxTo(value)
           try {
             console.log(`${ethers.getAddress(value)}`)
             setToBorderColor("border-gray-300")
@@ -36,7 +56,7 @@ export function Send() {
           }
           break
         case InputId.amount:
-          setAmount(value)
+          setTxValue(value)
           try {
             console.log(`${ethers.parseUnits(value, "ether")}`)
             setAmountBorderColor("border-gray-300")
@@ -54,7 +74,24 @@ export function Send() {
     []
   )
 
-  const handleSend = async () => {}
+  const handleSend = useCallback(async () => {
+    // const paymaster = createPaymaster(paymasterSelected)
+    const accountAddress = (await providerCtx.provider.listAccounts()).map(
+      (account) => account.address
+    )[providerCtx.index]
+
+    const signer = providerCtx.provider.getSigner()
+    const txResult = await (
+      await signer
+    ).sendTransaction({
+      from: accountAddress,
+      to: ethers.getAddress(txTo),
+      value: ethers.parseUnits(txValue.toString(), "ether")
+    })
+
+    // TODO: Need to avoid Popup closure
+    setTxHash(txResult.hash)
+  }, [providerCtx, txTo, txValue])
 
   return (
     <div className="text-base justify-center items-center h-auto">
@@ -63,16 +100,22 @@ export function Send() {
         <input
           type="text"
           id={`${InputId.to}`}
-          value={`${to}`}
+          value={`${txTo}`}
           onChange={inputChange}
+          list="suggestionTo"
           className={`border-2 ${toBorderColor} w-96 outline-none`}></input>
+        <datalist id="suggestionTo">
+          {txToSuggestions.map((to, index) => {
+            return <option key={index} value={to} />
+          })}
+        </datalist>
       </div>
       <div className="flex">
         <label className="flex-1">Amount:</label>
         <input
           type="text"
           id={`${InputId.amount}`}
-          value={`${amount}`}
+          value={`${txValue}`}
           onChange={inputChange}
           className={`border ${amountBorderColor} w-96 outline-none`}></input>
       </div>
@@ -84,8 +127,12 @@ export function Send() {
           Cancel
         </button>
       </div>
-
-      {/* <button onClick={handleTransfer}>Transfer</button> */}
+      {txHash && (
+        <div className="flex">
+          <label className="flex-1">TxHash:</label>
+          <span className="flex-1">{txHash}</span>
+        </div>
+      )}
     </div>
   )
 }
