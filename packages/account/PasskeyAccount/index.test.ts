ccea5f86b3b61958fe96a3402ff04111f073f447
pilagod <pilagooood@gmail.com>
2024-06-05 17:47:00 +0800

    fix passkey test

--- packages/account/PasskeyAccount/index.test.ts
+++ packages/account/PasskeyAccount/index.test.ts
@@ -1,6 +1,7 @@
 import config from "~config/test"
 import number from "~packages/util/number"
 import { describeAccountSuite } from "~packages/util/testing/suite/account"
+import { WaalletRpcMethod } from "~packages/waallet/rpc"
 
 import { PasskeyAccount } from "./index"
 import { PasskeyOwnerP256 } from "./passkeyOwnerP256"
@@ -21,16 +22,25 @@ describeAccountSuite({
       // TODO: This test at this moment relies on tests in test bed to deploy the account.
       // It would be better to decouple it.
       it("should init with existing passkey account", async () => {
+        // Use `initCode`` to deploy account
+        await ctx.provider.request({
+          method: WaalletRpcMethod.eth_sendTransaction,
+          params: [
+            {
+              to: await ctx.account.getAddress()
+            }
+          ]
+        })
         const { node } = config.networkManager.getActive()
-        const a = await PasskeyAccount.init(node, {
+        const account = await PasskeyAccount.init(node, {
           address: await ctx.account.getAddress(),
           owner
         })
-        expect(await a.getAddress()).toBe(await ctx.account.getAddress())
+        expect(await account.getAddress()).toBe(await ctx.account.getAddress())
 
         const credentialId = await PasskeyAccount.getCredentialId(
           node,
-          await a.getAddress()
+          await account.getAddress()
         )
         expect(credentialId).toBe(owner.getCredentialId())
       })
