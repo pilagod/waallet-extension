f8db0ec4125ff0f5e66641b90b72d3c92859e10e
pilagod <pilagooood@gmail.com>
2024-06-13 14:33:27 +0800

    add provider v0.7 tests

---
+++ packages/waallet/background/provider.v0_7.test.ts
@@ -0,0 +1,188 @@
+import * as ethers from "ethers"
+
+import { SimpleAccount } from "~packages/account/SimpleAccount"
+import { UserOperationV0_7 } from "~packages/bundler/userOperation/v0_7"
+import byte from "~packages/util/byte"
+import number from "~packages/util/number"
+import { describeWaalletSuite } from "~packages/util/testing/suite/waallet"
+import { WaalletRpcMethod } from "~packages/waallet/rpc"
+import type { HexString } from "~typing"
+
+describeWaalletSuite({
+  name: "WalletBackgroundProvider",
+  useAccount: (cfg) => {
+    return SimpleAccount.initWithFactory(cfg.provider.node, {
+      ownerPrivateKey: cfg.wallet.operator.privateKey,
+      factoryAddress: cfg.address.SimpleAccountFactoryV0_7,
+      salt: number.random()
+    })
+  },
+  suite: (ctx) => {
+    it("should get chain id", async () => {
+      const chainId = await ctx.provider.waallet.request<HexString>({
+        method: WaalletRpcMethod.eth_chainId
+      })
+      expect(parseInt(chainId, 16)).toBe(1337)
+    })
+
+    it("should get block number", async () => {
+      const blockNumber = await ctx.provider.waallet.request<HexString>({
+        method: WaalletRpcMethod.eth_blockNumber
+      })
+      expect(parseInt(blockNumber, 16)).toBeGreaterThan(0)
+    })
+
+    it("should get accounts", async () => {
+      const accounts = await ctx.provider.waallet.request<HexString>({
+        method: WaalletRpcMethod.eth_accounts
+      })
+      expect(accounts.length).toBeGreaterThan(0)
+      expect(accounts[0]).toHaveLength(42)
+    })
+
+    it("should request accounts", async () => {
+      const accounts = await ctx.provider.waallet.request<HexString>({
+        method: WaalletRpcMethod.eth_requestAccounts
+      })
+      expect(accounts.length).toBeGreaterThan(0)
+      expect(accounts[0]).toHaveLength(42)
+    })
+
+    it("should estimate gas", async () => {
+      await ctx.topupAccount()
+
+      const {
+        contract: { counter }
+      } = ctx
+
+      const gas = await ctx.provider.waallet.request<HexString>({
+        method: WaalletRpcMethod.eth_estimateGas,
+        params: [
+          {
+            to: await counter.getAddress(),
+            value: 1,
+            data: counter.interface.encodeFunctionData("increment", [])
+          }
+        ]
+      })
+      expect(byte.isHex(gas)).toBe(true)
+      expect(parseInt(gas, 16)).toBeGreaterThan(0)
+    })
+
+    it("should fail to estimate user operation when nonce doesn't match the one on chain", async () => {
+      await ctx.topupAccount()
+
+      const {
+        contract: { counter }
+      } = ctx
+
+      const userOpCall = await ctx.account.createUserOperationCall({
+        to: await counter.getAddress(),
+        value: 1,
+        data: counter.interface.encodeFunctionData("increment", [])
+      })
+      const userOp = new UserOperationV0_7({
+        ...userOpCall,
+        factory: ethers.dataSlice(userOpCall.initCode, 0, 20),
+        factoryData: ethers.dataSlice(userOpCall.initCode, 20)
+      })
+      // Use custom nonce which doesn't match the one on chain
+      userOp.setNonce(userOp.nonce + 1n)
+
+      const useInvalidNonce = async () =>
+        ctx.provider.waallet.request<{
+          preVerificationGas: HexString
+          verificationGasLimit: HexString
+          callGasLimit: HexString
+        }>({
+          method: WaalletRpcMethod.eth_estimateUserOperationGas,
+          params: [userOp.data(), await ctx.account.getEntryPoint()]
+        })
+
+      await expect(useInvalidNonce()).rejects.toThrow()
+    })
+
+    it("should send transaction to contract", async () => {
+      await ctx.topupAccount()
+
+      const {
+        contract: { counter },
+        provider: { node }
+      } = ctx
+
+      const balanceBefore = await node.getBalance(counter.getAddress())
+      const counterBefore = (await counter.number()) as bigint
+
+      const txHash = await ctx.provider.waallet.request<HexString>({
+        method: WaalletRpcMethod.eth_sendTransaction,
+        params: [
+          {
+            to: await counter.getAddress(),
+            value: 1,
+            data: counter.interface.encodeFunctionData("increment", [])
+          }
+        ]
+      })
+      const receipt = await node.getTransactionReceipt(txHash)
+      expect(receipt.status).toBe(1)
+
+      const balanceAfter = await node.getBalance(counter.getAddress())
+      expect(balanceAfter - balanceBefore).toBe(1n)
+
+      const counterAfter = (await counter.number()) as bigint
+      expect(counterAfter - counterBefore).toBe(1n)
+    })
+
+    it.only("should send user operation", async () => {
+      await ctx.topupAccount()
+
+      const {
+        contract: { counter },
+        provider: { node, bundler }
+      } = ctx
+
+      const counterBefore = (await counter.number()) as bigint
+      const counterBalanceBefore = await node.getBalance(
+        await counter.getAddress()
+      )
+
+      const userOpCall = await ctx.account.createUserOperationCall({
+        to: await counter.getAddress(),
+        value: 1,
+        data: counter.interface.encodeFunctionData("increment", [])
+      })
+      const userOp = new UserOperationV0_7({
+        ...userOpCall,
+        factory: ethers.dataSlice(userOpCall.initCode, 0, 20),
+        factoryData: ethers.dataSlice(userOpCall.initCode, 20)
+      })
+
+      const { gasPrice } = await node.getFeeData()
+      userOp.setGasFee(gasPrice * 2n)
+
+      const entryPoint = await ctx.account.getEntryPoint()
+      userOp.setGasLimit(
+        await bundler.estimateUserOperationGas(userOp, entryPoint)
+      )
+
+      const chainId = await bundler.getChainId()
+      userOp.setSignature(
+        await ctx.account.sign(userOp.hash(entryPoint, chainId))
+      )
+
+      const userOpHash = await ctx.provider.waallet.request<HexString>({
+        method: WaalletRpcMethod.eth_sendUserOperation,
+        params: [userOp.data(), entryPoint]
+      })
+      await bundler.wait(userOpHash)
+
+      const counterAfter = (await counter.number()) as bigint
+      expect(counterAfter - counterBefore).toBe(1n)
+
+      const counterBalanceAfter = await node.getBalance(
+        await counter.getAddress()
+      )
+      expect(counterBalanceAfter - counterBalanceBefore).toBe(1n)
+    })
+  }
+})
