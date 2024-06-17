10401d95a40e90f7cd931fc87a2e9f74e039002c
Irara <irara.chen@token.im>
2024-06-17 15:09:25 +0800

    chg: Use the hashMessage to sign message

--- packages/util/cryptography/index.ts
+++ packages/util/cryptography/index.ts
@@ -1,23 +1,11 @@
-import { isoUint8Array } from "@simplewebauthn/server/helpers"
-import {
-  concat,
-  getBytes,
-  isBytesLike,
-  keccak256,
-  toUtf8Bytes,
-  type BytesLike
-} from "ethers"
+import { getBytes, hashMessage, isBytesLike, type BytesLike } from "ethers"
 
 function toEthSignedMessageHash(message: BytesLike): Uint8Array {
   if (!isBytesLike(message)) {
     throw new Error("Invalid message")
   }
 
-  const messageUint8Array = getBytes(message)
-  const prefixBytes = toUtf8Bytes(
-    `\x19Ethereum Signed Message:\n${messageUint8Array.byteLength.toString()}`
-  )
-  return getBytes(keccak256(concat([prefixBytes, messageUint8Array])))
+  return getBytes(hashMessage(message))
 }
 
 export default { toEthSignedMessageHash }
