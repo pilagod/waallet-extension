887f926badc79f23296f321edc4964f53b577fb5
Irara <irara.chen@token.im>
2024-06-18 17:05:54 +0800

    chg: Add signature verification test

--- packages/account/PasskeyAccount/passkeyOwnerP256.test.ts
+++ packages/account/PasskeyAccount/passkeyOwnerP256.test.ts
@@ -1,4 +1,8 @@
-import { toUtf8Bytes } from "ethers"
+import { p256 } from "@noble/curves/p256"
+import { isoBase64URL } from "@simplewebauthn/server/helpers"
+import { AbiCoder, getBytes, hashMessage } from "ethers"
+
+import byte from "~packages/util/byte"
 
 import { PasskeyOwnerP256 } from "./passkeyOwnerP256"
 
@@ -9,5 +13,43 @@ describe("PasskeyOwnerP256", () => {
     const challenge = "challenge"
     const signature = await owner.sign(challenge)
     console.log(signature)
+  })
+
+  it("should verify signature", async () => {
+    const challenge = "challenge"
+    const signature = await owner.sign(challenge)
+    const signatureDecoded = AbiCoder.defaultAbiCoder().decode(
+      [
+        "bool", // isSimulation
+        "bytes", // authenticatorData
+        "bool", // requireUserVerification
+        "string", // clientDataJSON
+        "uint256", // challengeLocation
+        "uint256", // responseTypeLocation
+        "uint256", // r
+        "uint256" // s
+      ],
+      signature
+    )
+
+    expect(isoBase64URL.fromBuffer(getBytes(hashMessage(challenge)))).toBe(
+      JSON.parse(signatureDecoded[3]).challenge
+    )
+
+    const message = byte
+      .sha256(
+        `${signatureDecoded[1]}${byte
+          .sha256(signatureDecoded[3])
+          .toString("hex")}`
+      )
+      .toString("hex")
+
+    expect(
+      p256.verify(
+        { r: signatureDecoded[6], s: signatureDecoded[7] },
+        message,
+        owner.publicKey
+      )
+    ).toBe(true)
   })
 })
