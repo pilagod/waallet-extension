da7e4476db0311c3c79aa0fafdc46f717e5775b0
Irara <irara.chen@token.im>
2024-06-19 10:30:32 +0800

    chg: Simplify test code

--- packages/account/PasskeyAccount/passkeyOwnerP256.test.ts
+++ packages/account/PasskeyAccount/passkeyOwnerP256.test.ts
@@ -18,38 +18,31 @@ describe("PasskeyOwnerP256", () => {
   it("should verify signature", async () => {
     const challenge = "challenge"
     const signature = await owner.sign(challenge)
-    const signatureDecoded = AbiCoder.defaultAbiCoder().decode(
-      [
-        "bool", // isSimulation
-        "bytes", // authenticatorData
-        "bool", // requireUserVerification
-        "string", // clientDataJSON
-        "uint256", // challengeLocation
-        "uint256", // responseTypeLocation
-        "uint256", // r
-        "uint256" // s
-      ],
-      signature
-    )
+    const [, authenticatorData, , clientDataJSON, , , r, s] =
+      AbiCoder.defaultAbiCoder().decode(
+        [
+          "bool", // isSimulation
+          "bytes", // authenticatorData
+          "bool", // requireUserVerification
+          "string", // clientDataJSON
+          "uint256", // challengeLocation
+          "uint256", // responseTypeLocation
+          "uint256", // r
+          "uint256" // s
+        ],
+        signature
+      )
 
     expect(isoBase64URL.fromBuffer(getBytes(hashMessage(challenge)))).toBe(
-      JSON.parse(signatureDecoded[3]).challenge
+      JSON.parse(clientDataJSON).challenge
     )
 
     const message = byte
       .sha256(
-        `${signatureDecoded[1]}${byte
-          .sha256(signatureDecoded[3])
-          .toString("hex")}`
+        `${authenticatorData}${byte.sha256(clientDataJSON).toString("hex")}`
       )
       .toString("hex")
 
-    expect(
-      p256.verify(
-        { r: signatureDecoded[6], s: signatureDecoded[7] },
-        message,
-        owner.publicKey
-      )
-    ).toBe(true)
+    expect(p256.verify({ r, s }, message, owner.publicKey)).toBe(true)
   })
 })
