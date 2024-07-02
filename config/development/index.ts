7857ae272140186012c35f9ddbe50eaedb065464
pilagod <pilagooood@gmail.com>
2024-07-02 16:06:30 +0800

    add polygon amoy network

--- config/development/index.ts
+++ config/development/index.ts
@@ -38,6 +38,23 @@ export const config = {
       }
     },
     {
+      chainId: 80002,
+      name: "Polygon Amoy",
+      active: accountChainId === 80002,
+      nodeRpcUrl: `https://polygon-amoy.g.alchemy.com/v2/${process.env.PLASMO_PUBLIC_SEPOLIA_ALCHEMY_API_KEY}`,
+      bundlerRpcUrl: `https://polygon-amoy.g.alchemy.com/v2/${process.env.PLASMO_PUBLIC_SEPOLIA_ALCHEMY_API_KEY}`,
+      entryPoint: {
+        [EntryPointVersion.V0_6]: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
+        [EntryPointVersion.V0_7]: "0x0000000071727De22E5E9d8BAf0edAc6f37da032"
+      },
+      accountFactory: {
+        [AccountType.SimpleAccount]:
+          "0x5e85DD2Eb66CD7C92Ed469336Dc58d8F7efa62Cc", // v0.7
+        [AccountType.PasskeyAccount]:
+          "0xccd0AE1467e4A70528f2c2A9E63800Acfec506dE" // v0.7
+      }
+    },
+    {
       chainId: 1337,
       name: "Local",
       active: accountChainId === 1337,
