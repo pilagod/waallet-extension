3a7c82f8b8d291ffbaae3301f4812ca7e1439d06
Irara <irara.chen@token.im>
2024-07-10 18:23:12 +0800

    refac: Render UserOpHistoryItem with Log component

--- app/page/home/activity.tsx
+++ app/page/home/activity.tsx
@@ -1,9 +1,6 @@
 import ArrowRightArrowLeft from "react:~assets/arrowRightArrowLeft.svg"
 import ArrowUpRight from "react:~assets/arrowUpRight.svg"
-import CircleQuestion from "react:~assets/circleQuestion.svg"
 import CircleXmark from "react:~assets/circleXmark.svg"
-import Clock from "react:~assets/clock.svg"
-import OctagonXmark from "react:~assets/octagonXmark.svg"
 
 import { useAccount, useNetwork, useTransactionLogs } from "~app/storage"
 import { decodeExecuteParams, decodeTransferParams } from "~app/util/calldata"
@@ -56,182 +53,30 @@ const UserOpHistoryItem: React.FC<{
     second: "numeric",
     hour12: false
   })
+  const callData = txLog.detail.data.callData
 
   if (txLog.status === TransactionStatus.Succeeded) {
     const userOpHash = txLog.receipt.userOpHash
     return (
       <a
-        className="w-full flex items-center p-[14px_0px_14px_0px]"
+        className="w-full flex items-center"
         href={`${explorerUrl}userOpHash/${userOpHash}?network=${chainName}`}
         target="_blank">
         {/* Status and to address */}
-        <div className="flex-grow flex flex-col items-start">
-          {/* Status and token amount */}
-          <div className="flex items-center mb-[4px]">
-            <ArrowUpRight className="w-[16px] h-[16px] mr-[8px]" />
-            <div className="mr-[8px] leading-[19px] text-[16px] text-[#000000]">
-              Send
-            </div>
-            <div className="mr-[8px] leading-[19px] text-[16px] text-[#000000]">
-              1.2
-            </div>
-            <div className="leading-[19px] text-[16px] text-[#000000]">ETH</div>
-          </div>
-          {/* To address */}
-          <div className="leading-[19px] text-[16px] text-[#bbbbbb] whitespace-nowrap">
-            to: 0x0000...00000
-          </div>
-        </div>
+        <Log calldata={callData} txStatus={txLog.status} />
         {/* Transaction time */}
-        <div>
-          <div className="flex flex-col items-end">
-            <div className="mb-[4px] leading-[19px] text-[16px] text-[#000000]">
-              {creationDate}
-            </div>
-            <div className="leading-[19px] text-[16px] text-[#000000]">
-              {creationTime}
-            </div>
-          </div>
-        </div>
+        <TransactionTime time={creationTime} date={creationDate} />
       </a>
-    )
-  }
-
-  if (txLog.status === TransactionStatus.Failed) {
-    const userOpHash = txLog.receipt.userOpHash
-    return (
-      <a
-        className="w-full flex items-center p-[14px_0px_14px_0px]"
-        href={`${explorerUrl}userOpHash/${userOpHash}?network=${chainName}`}
-        target="_blank">
-        {/* Status and to address */}
-        <div className="flex-grow flex flex-col items-start">
-          {/* Status and token amount */}
-          <div className="flex items-center mb-[4px]">
-            <OctagonXmark className="w-[16px] h-[16px] mr-[8px]" />
-            <div className="mr-[8px] leading-[19px] text-[16px] text-[#000000]">
-              Failed
-            </div>
-            <div className="mr-[8px] leading-[19px] text-[16px] text-[#000000]">
-              1.2
-            </div>
-            <div className="leading-[19px] text-[16px] text-[#000000]">ETH</div>
-          </div>
-          {/* To address */}
-          <div className="leading-[19px] text-[16px] text-[#bbbbbb] whitespace-nowrap">
-            to: 0x0000...00000
-          </div>
-        </div>
-        {/* Transaction time */}
-        <div>
-          <div className="flex flex-col items-end">
-            <div className="mb-[4px] leading-[19px] text-[16px] text-[#000000]">
-              {creationDate}
-            </div>
-            <div className="leading-[19px] text-[16px] text-[#000000]">
-              {creationTime}
-            </div>
-          </div>
-        </div>
-      </a>
-    )
-  }
-
-  if (txLog.status === TransactionStatus.Rejected) {
-    return (
-      <div className="w-full flex items-center p-[14px_0px_14px_0px]">
-        <div className="flex-grow flex flex-col items-start">
-          {/* Status and image */}
-          <div className="flex items-center">
-            <OctagonXmark className="w-[16px] h-[16px] mr-[8px]" />
-            <div className="mr-[8px] leading-[19px] text-[16px] text-[#000000]">
-              {status}
-            </div>
-          </div>
-        </div>
-        {/* Transaction time */}
-        <div>
-          <div className="flex flex-col items-end">
-            <div className="mb-[4px] leading-[19px] text-[16px] text-[#000000]">
-              {creationDate}
-            </div>
-            <div className="leading-[19px] text-[16px] text-[#000000]">
-              {creationTime}
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-  if (txLog.status === TransactionStatus.Sent) {
-    return (
-      <div className="w-full flex items-center p-[14px_0px_14px_0px]">
-        {/* Status and to address */}
-        <div className="flex-grow flex flex-col items-start">
-          {/* Status and token amount */}
-          <div className="flex items-center mb-[4px]">
-            <Clock className="w-[16px] h-[16px] mr-[8px]" />
-            <div className="mr-[8px] leading-[19px] text-[16px] text-[#000000]">
-              {status}
-            </div>
-            <div className="mr-[8px] leading-[19px] text-[16px] text-[#000000]">
-              1.2
-            </div>
-            <div className="leading-[19px] text-[16px] text-[#000000]">ETH</div>
-          </div>
-          {/* To address */}
-          <div className="leading-[19px] text-[16px] text-[#bbbbbb] whitespace-nowrap">
-            to: 0x0000...00000
-          </div>
-        </div>
-        {/* Transaction time */}
-        <div>
-          <div className="flex flex-col items-end">
-            <div className="mb-[4px] leading-[19px] text-[16px] text-[#000000]">
-              {creationDate}
-            </div>
-            <div className="leading-[19px] text-[16px] text-[#000000]">
-              {creationTime}
-            </div>
-          </div>
-        </div>
-      </div>
     )
   }
 
   // Others' case
   return (
-    <div className="w-full flex items-center p-[14px_0px_14px_0px]">
+    <div className="w-full flex items-center">
       {/* Status and to address */}
-      <div className="flex-grow flex flex-col items-start">
-        {/* Status and token amount */}
-        <div className="flex items-center mb-[4px]">
-          <CircleQuestion className="w-[16px] h-[16px] mr-[8px]" />
-          <div className="mr-[8px] leading-[19px] text-[16px] text-[#000000]">
-            {status}
-          </div>
-          <div className="mr-[8px] leading-[19px] text-[16px] text-[#000000]">
-            1.2
-          </div>
-          <div className="leading-[19px] text-[16px] text-[#000000]">ETH</div>
-        </div>
-        {/* To address */}
-        <div className="leading-[19px] text-[16px] text-[#bbbbbb] whitespace-nowrap">
-          to: 0x0000...00000
-        </div>
-      </div>
+      <Log calldata={callData} txStatus={txLog.status} />
       {/* Transaction time */}
-      <div>
-        <div className="flex flex-col items-end">
-          <div className="mb-[4px] leading-[19px] text-[16px] text-[#000000]">
-            {creationDate}
-          </div>
-          <div className="leading-[19px] text-[16px] text-[#000000]">
-            {creationTime}
-          </div>
-        </div>
-      </div>
+      <TransactionTime time={creationTime} date={creationDate} />
     </div>
   )
 }
