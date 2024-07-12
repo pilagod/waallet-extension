b4fa9b7daa3f03f7a6b098cd361c7d4277235307
WIN-LIN <puppy95176@gmail.com>
2024-07-12 11:13:50 +0800

    remove setToast from state storage

--- app/storage/slice/state.ts
+++ app/storage/slice/state.ts
@@ -1,21 +1,11 @@
-import type { ToastStatus } from "~app/component/toast"
 import type { State } from "~storage/local/state"
 
 import type { BackgroundStateCreator } from "../middleware/background"
 
 export interface StateSlice {
   state: State
-  setToast: (message: string, status: ToastStatus) => Promise<void>
 }
 
 export const createStateSlice: BackgroundStateCreator<StateSlice> = (set) => ({
-  state: null,
-  setToast: async (message, status) => {
-    await set(({ state }) => {
-      state.toast = {
-        message,
-        status
-      }
-    })
-  }
+  state: null
 })
