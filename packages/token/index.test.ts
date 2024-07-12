5768ce1eaa9cc679c15fe2b7fa87e3a62720b2ab
Irara <irara.chen@token.im>
2024-07-12 16:46:12 +0800

    doc: Add the TokenContract test

--- packages/token/index.test.ts
+++ packages/token/index.test.ts
@@ -1,26 +1,64 @@
-import { getAddress, Interface, parseUnits, zeroPadValue } from "ethers"
+import { Contract, getAddress, parseUnits, zeroPadValue } from "ethers"
 
-import { Token } from "~packages/token"
+import config from "~config/test"
+import { TokenContract } from "~packages/token"
+import number from "~packages/util/number"
 
-describe("calldata", () => {
-  const accountAbi: string[] = [
-    "function transfer(address to, uint256 value) public returns (bool)"
-  ]
-  const toAddress = getAddress(zeroPadValue("0x1234", 20))
+describe("Token contract", () => {
+  let token: TokenContract
+  let mintableToken: Contract
+
+  const fromAddress = getAddress(zeroPadValue("0x1234", 20))
+  const toAddress = getAddress(zeroPadValue("0x5678", 20))
   const tokenValue = 0.1
   const tokenDecimals = 18
 
-  const transferIface = new Interface(accountAbi)
+  beforeAll(async () => {
+    token = await TokenContract.init(
+      config.address.TestToken,
+      config.provider.node
+    )
 
-  const transferCalldata = transferIface.encodeFunctionData("transfer", [
-    toAddress,
-    parseUnits(tokenValue.toString(), tokenDecimals)
-  ])
+    mintableToken = new Contract(
+      config.address.TestToken,
+      ["function mint(address to, uint256 value)"],
+      config.wallet.operator
+    )
+  })
 
-  it("should decode the token transfer calldata", () => {
-    const { to, value } = Token.decodeTransferParam(transferCalldata)
+  it("should encode and decode the token transfer calldata", () => {
+    const transferCalldata = TokenContract.encodeTransferData(
+      toAddress,
+      parseUnits(tokenValue.toString(), tokenDecimals)
+    )
+
+    const { to, value } = TokenContract.decodeTransferParam(transferCalldata)
 
     expect(to).toBe(toAddress)
     expect(value).toBe(parseUnits(tokenValue.toString(), tokenDecimals))
+  })
+
+  it("should encode and decode the token transfer from calldata", () => {
+    const transferFromCalldata = TokenContract.encodeTransferFromData(
+      fromAddress,
+      toAddress,
+      parseUnits(tokenValue.toString(), tokenDecimals)
+    )
+
+    const { from, to, value } =
+      TokenContract.decodeTransferFromParam(transferFromCalldata)
+
+    expect(from).toBe(fromAddress)
+    expect(to).toBe(toAddress)
+    expect(value).toBe(parseUnits(tokenValue.toString(), tokenDecimals))
+  })
+
+  it("should get balance", async () => {
+    const amount = parseUnits("100", token.decimals)
+    const balanceBefore = number.toBigInt(await token.balanceOf(fromAddress))
+    await mintableToken.mint(fromAddress, amount)
+    const balanceAfter = number.toBigInt(await token.balanceOf(fromAddress))
+
+    expect(balanceAfter - balanceBefore).toBe(amount)
   })
 })
