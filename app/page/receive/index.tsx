d0a6fc36dab148257f29391eb626f4accf8ac203
Irara <irara.chen@token.im>
2024-07-18 15:04:48 +0800

    chg: Resolve the lint error

--- app/page/receive/index.tsx
+++ app/page/receive/index.tsx
@@ -24,7 +24,7 @@ export const Receive = () => {
 
   return (
     <>
-      <StepBackHeader title={"Jesse's wallet"} href={Path.Index} />
+      <StepBackHeader title={"Jesse's wallet"} />
       <div className="flex items-center justify-center my-[24px] py-[24px]">
         <QrCode address={address} />
       </div>
