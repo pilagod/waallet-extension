520f427edb0b1984e40e5233e5dddba53ebe6294
Irara <irara.chen@token.im>
2024-07-18 14:22:22 +0800

    Add the 'copy' button handler

--- app/page/receive/index.tsx
+++ app/page/receive/index.tsx
@@ -1,14 +1,26 @@
+import { useContext } from "react"
 import Copy from "react:~assets/copy.svg"
 import Wallet from "react:~assets/wallet.svg"
 
 import { Divider } from "~app/component/divider"
 import { QrCode } from "~app/component/qrcode"
 import { StepBackHeader } from "~app/component/stepBackHeader"
+import { ToastContext } from "~app/context/toastContext"
 import { Path } from "~app/path"
 import { useAccount } from "~app/storage"
 
 export const Receive = () => {
   const { address } = useAccount()
+  const { setToast } = useContext(ToastContext)
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(address)
+      setToast("Copy address to clipboard.", "success")
+    } catch (e) {
+      new Error(`Failed to copy text: ${e.message}`)
+    }
+  }
 
   return (
     <>
@@ -29,7 +41,9 @@ export const Receive = () => {
           </div>
         </div>
         {/* Copy button */}
-        <button className="flex items-center p-[16px_104.5px_16px_104.5px] my-[22.5px] rounded-full border-[1px] border-solid border-black bg-black">
+        <button
+          className="flex items-center p-[16px_104.5px_16px_104.5px] my-[22.5px] rounded-full border-[1px] border-solid border-black bg-black"
+          onClick={handleCopy}>
           <Copy className="w-[24px] h-[24px] mr-[8px]" />
           <div className="text-[18px] text-[#FFFFFF] whitespace-nowrap">
             Copy address
