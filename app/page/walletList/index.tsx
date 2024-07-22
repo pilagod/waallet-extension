14f90b6780917db72b42c2b055042aa25a231729
pilagod <pilagooood@gmail.com>
2024-07-22 16:40:44 +0800

    add switch account in wallet list

--- app/page/walletList/index.tsx
+++ app/page/walletList/index.tsx
@@ -1,27 +1,38 @@
 import { useState } from "react"
 import Plus from "react:~assets/plus"
 import Wallet from "react:~assets/wallet"
+import { useHashLocation } from "wouter/use-hash-location"
 
 import { Divider } from "~app/component/divider"
 import { PasskeyVerification } from "~app/component/passkeyVerification"
 import { StepBackHeader } from "~app/component/stepBackHeader"
-import { useAccounts } from "~app/hook/storage"
+import { useAccounts, useAction } from "~app/hook/storage"
+import { Path } from "~app/path"
 import address from "~packages/util/address"
 import number from "~packages/util/number"
 import type { HexString } from "~typing"
 
 export function WalletList() {
+  const [, navigate] = useHashLocation()
+  const { switchAccount } = useAction()
+
   const accounts = useAccounts()
   const totalBalance = accounts.reduce((b, a) => {
     return b + number.toBigInt(a.balance)
   }, 0n)
+
   const [walletCreating, setWalletCreating] = useState(false)
   const createWallet = () => {
     setWalletCreating(true)
   }
+  const selectWallet = async (accountId: string) => {
+    await switchAccount(accountId)
+    navigate(Path.Home)
+  }
   if (walletCreating) {
     return <PasskeyVerification purpose="identity" />
   }
+
   return (
     <>
       <StepBackHeader title="Wallet List" />
@@ -36,12 +47,17 @@ export function WalletList() {
 
       {/* Wallet List */}
       <section className="h-[264px] overflow-x-hidden overflow-y-scroll">
-        {accounts.map((a) => {
+        {accounts.map((a, i) => {
           return (
-            <WalletItem
-              address={a.address}
-              balance={number.toBigInt(a.balance)}
-            />
+            <div
+              key={i}
+              className="cursor-pointer"
+              onClick={() => selectWallet(a.id)}>
+              <WalletItem
+                address={a.address}
+                balance={number.toBigInt(a.balance)}
+              />
+            </div>
           )
         })}
       </section>
@@ -67,7 +83,7 @@ function WalletItem(props: { address: HexString; balance: bigint }) {
   // Effect of `min-w-0`:
   // https://stackoverflow.com/questions/36230944/prevent-flex-items-from-overflowing-a-container
   return (
-    <div className="flex flex-row items-center gap-[12px] py-[16px] text-[16px] cursor-pointer">
+    <div className="flex flex-row items-center gap-[12px] py-[16px] text-[16px]">
       <div className="basis-[24px]">
         <Wallet />
       </div>
