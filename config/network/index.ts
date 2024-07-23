4478a4e4f8a00b094c2cf685b3b050d656c4782f
pilagod <pilagooood@gmail.com>
2024-07-23 15:18:21 +0800

    add network config

---
+++ config/network/index.ts
@@ -0,0 +1,17 @@
+export default {
+  [1337]: {
+    chainId: 1337,
+    name: "Geth Testnet",
+    icon: "~assets/ethereum.svg"
+  },
+  [80002]: {
+    chainId: 80002,
+    name: "Polygon Amoy",
+    icon: "~assets/polygon.svg"
+  },
+  [11155111]: {
+    chainId: 11155111,
+    name: "Sepolia",
+    icon: "~assets/ethereum.svg"
+  }
+}
