2fb981c5b74e424464b81625422cfb1b67eaefb4
pilagod <pilagooood@gmail.com>
2024-07-23 15:37:00 +0800

    add switch network action in network list

--- app/page/networkList/index.tsx
+++ app/page/networkList/index.tsx
@@ -1,21 +1,34 @@
 import Circle from "react:~assets/circle.svg"
 import CircleDot from "react:~assets/circleDot.svg"
+import { useHashLocation } from "wouter/use-hash-location"
 
 import { StepBackHeader } from "~app/component/stepBackHeader"
-import { useNetwork, useNetworks } from "~app/hook/storage"
+import { useAction, useNetwork, useNetworks } from "~app/hook/storage"
+import { Path } from "~app/path"
 
 export function NetworkList() {
+  const [, navigate] = useHashLocation()
+  const { switchNetwork } = useAction()
+
   const network = useNetwork()
   const networks = useNetworks()
+  const selectNetwork = async (networkId: string) => {
+    await switchNetwork(networkId)
+    navigate(Path.Home)
+  }
+
   return (
     <>
       <StepBackHeader title="Select Network" />
 
       {/* Network List */}
-      <section className="pt-[16px]">
+      <section className="w-[calc(100%+32px)] ml-[-16px] pt-[16px]">
         {networks.map((n, i) => {
           return (
-            <div key={i}>
+            <div
+              key={i}
+              className="cursor-pointer hover:bg-[#F5F5F5]"
+              onClick={() => selectNetwork(n.id)}>
               <NetworkItem
                 name={n.name}
                 icon={n.icon}
@@ -31,7 +44,7 @@ export function NetworkList() {
 
 function NetworkItem(props: { name: string; icon: string; active: boolean }) {
   return (
-    <div className="flex flex-row items-center gap-[12px] py-[16px]">
+    <div className="flex flex-row items-center gap-[12px] p-[16px]">
       <div className="basis-[36px]">
         <img src={props.icon} alt={props.name} />
       </div>
