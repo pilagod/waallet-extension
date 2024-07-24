d3d3abbec2e6edc25909ed34b7bab2d4293c4b7c
WIN-LIN <puppy95176@gmail.com>
2024-07-24 16:25:47 +0800

    send error msg to app

--- background/messages/JsonRpcRequest.ts
+++ background/messages/JsonRpcRequest.ts
@@ -1,6 +1,9 @@
+import { format } from "util"
+
 import { type PlasmoMessaging } from "@plasmohq/messaging"
 
 import { getWaalletBackgroundProvider } from "~background/provider"
+import { JsonRpcProviderError } from "~packages/rpc/json/error"
 import { WaalletMessage } from "~packages/waallet/message"
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
@@ -8,13 +11,28 @@ const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
     `[background][message][${WaalletMessage.JsonRpcRequest}][request]`,
     req
   )
-  const provider = getWaalletBackgroundProvider()
-  const result = await provider.request(req.body)
-  console.log(
-    `[background][message][${WaalletMessage.JsonRpcRequest}][response]`,
-    result
-  )
-  res.send(result)
+  try {
+    const provider = getWaalletBackgroundProvider()
+    const result = await provider.request(req.body)
+    console.log(
+      `[background][message][${WaalletMessage.JsonRpcRequest}][response]`,
+      result
+    )
+    res.send(result)
+  } catch (error) {
+    if (error instanceof JsonRpcProviderError) {
+      console.log(
+        `[background][message][${WaalletMessage.JsonRpcRequest}][JsonRpcProviderError]`,
+        error.unwrap()
+      )
+      res.send(error.unwrap())
+    }
+    console.log(
+      "[background][message][JsonRpcRequest][InternalError]",
+      format(error)
+    )
+    res.send(error)
+  }
 }
 
 export default handler
